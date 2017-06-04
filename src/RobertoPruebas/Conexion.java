3731e730732952073066ae9f110710e30e77f2b8
robertoar14 <arriola.rock97@gmail.com>
2017-06-03 21:05:36 -0600

    *Inclusion de metodos para manejo de productos en clase conexion *Inclusion del panel Productos

--- src/RobertoPruebas/Conexion.java
+++ src/RobertoPruebas/Conexion.java
@@ -5,6 +5,7 @@
  */
 package RobertoPruebas;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 /**
@@ -31,7 +32,7 @@ public class Conexion {
             } catch (ClassNotFoundException ex) {
             System.out.println("Error al registrar el driver de MySQL: " + ex);
         }
-            conexion=DriverManager.getConnection(url+ip+"/"+"rm_db",user,"blackdiamond");//Se conecta con la base de datos enviando
+            conexion=DriverManager.getConnection(url+ip+"/"+"mydb",user,"blackdiamond");//Se conecta con la base de datos enviando
             //los parametros url, user, pass,
             
         } catch (SQLException ex) {
@@ -50,5 +51,63 @@ public class Conexion {
         }
         conexion.close();
     }
+    public void insertarProducto (String codigo, String codBarras,String descrip, double venta, double costo, String estanteria, String columna, String fila, String marca, String unidad, int sucursal) throws SQLException{
+        conectar(); //permite la conexion con la base de datos
+        int marcaId=0;
+        int fraccion=0;
+        if(estanteria.isEmpty())
+            estanteria=null;
+        if(columna.isEmpty())
+            columna=null;
+        if(fila.isEmpty())
+            fila=null;
+        if(unidad!=null)
+            fraccion=1;
+        Statement instruccion=conexion.createStatement(); //Crea una nueva instruccion para la base de datos
+        ResultSet resultado = instruccion.executeQuery("select id from marca where Nombre= '"+marca.toUpperCase()+"';"); //se guarda el resultado de la instruccion, en esta ocasion, es una consulta
+        if(resultado.next())
+            marcaId=resultado.getInt(1);
+        else
+        {
+            instruccion=conexion.createStatement();
+            instruccion.executeUpdate("insert into marca (Nombre) values ('"+marca.toUpperCase()+"');");
+            resultado = instruccion.executeQuery("select id from marca where Nombre= '"+marca.toUpperCase()+"';");
+            marcaId=resultado.getInt(1);       
+        }
+       
+        instruccion.executeUpdate("insert into producto (codigo,codigo_barras,descripcion,precio_venta,precio_costo,Estanteria,"
+                +"Columna,Fila,marca_id,unidad_id,fraccion) values ('"+codigo+"','"+codBarras+"','"+descrip+"',"+venta+","+costo+","+estanteria+","+
+                columna+","+fila+","+marcaId+","+unidad+","+fraccion+");");
+        conexion.close();
+    }
+    /**
+     * Funcion que retorna guarda las sucursales y su id en una matriz 
+     * @return matriz dinamica de tamaño 2xN que alamcena la sucursal y su id
+     * @throws SQLException 
+     */
+    public ArrayList[] obtener_Sucursales_Unidades() throws SQLException{
+        ArrayList[] matriz=new ArrayList[4];
+        matriz[0]=new ArrayList();
+        matriz[1]=new ArrayList();
+        matriz[2]=new ArrayList();
+        matriz[3]=new ArrayList();
+        conectar(); //permite la conexion con la base de datos
+        Statement instruccion=conexion.createStatement(); //Crea una nueva instruccion para la base de datos
+        ResultSet resultado = instruccion.executeQuery("select id,Nombre from sucursales"); //se guarda el resultado de la instruccion, en esta ocasion, es una consulta
+        while(resultado.next())//Es una funcion booleana que mueve el cursor del resultado, si este es TRUE, aun hay registros de resultado
+        {
+            matriz[0].add(resultado.getInt(1));
+            matriz[1].add(resultado.getString(2));
+        }
+        resultado = instruccion.executeQuery("select id,Nombre from unidad"); //se guarda el resultado de la instruccion, en esta ocasion, es una consulta
+        while(resultado.next())//Es una funcion booleana que mueve el cursor del resultado, si este es TRUE, aun hay registros de resultado
+        {
+            matriz[2].add(resultado.getInt(1));
+            matriz[3].add(resultado.getString(2));
+        }
+        conexion.close();
+        return matriz;
+    }
+
 
 }
